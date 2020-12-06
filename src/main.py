5204863113daf26c99df3f3755b02eb625d4d6ec
Iman-Budi Pranakaish <ibpranakasih@gmail.com>
2020-12-07 02:50:58 +0700

    simplify SGD

--- src/main.py
+++ src/main.py
@@ -4,8 +4,7 @@ import load_mnist
 training_data ,validation_data , test_data =  load_mnist.load_preprocess_data()
 net = network.Network([784,30,10])
 
-net.SGD(training_data, 5,1000,3.0,test_data=test_data)
+net.SGD(training_data, 15,10,3.0,test_data=test_data)
 
 import model_to_json
-
 model_to_json.export(net)
